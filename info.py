e25cf4d593bb34fa0a9c908a40314420dec6e3f3
Sagnik Chakraborty <sagnikgraviton847@gmail.com>
2025-03-23 15:37:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -11,22 +11,38 @@ def is_enabled(value, default):
     else:
         return default
 #---------------------------------------------------------------
-#---------------------------------------------------------------         ,
+#---------------------------------------------------------------  
+[[[BOT_TOKEN: 
+
+API_ID: 
+
+API_HASH: 
+
+CHANNEL: 2374228840
+
+ADMINS: 
+
+DATABASE_URI:
+
+DATABASE NAME:
+
+LOG_CHANNEL: ]]]
+       ,
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '21484'))
-API_HASH = environ.get('API_HASH', 'db6286a041adf07ef4244e')
-BOT_TOKEN = environ.get('BOT_TOKEN', '7807I9ygow9sLqNQuI7hsWPm5FLNfBr9c')
+API_ID = int(environ.get('API_ID', '23294809'))
+API_HASH = environ.get('API_HASH', 'fbe5152195458e6aa8ec4298c96fb274')
+BOT_TOKEN = environ.get('BOT_TOKEN', '7941997498:AAEFqrwWqc7A6hYmKZxh9kcfAUGF-kotyeQ')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5665677555').split()]
-USERNAME = environ.get('USERNAME', "https://t.me/Innocent_babe_dead") # ADMIN USERNAME
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002190681742'))
-MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+K2kgCBgaat80YWQ9')
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002190681742').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1226915127').split()]
+USERNAME = environ.get('USERNAME', "https://t.me/MrEccentric") # ADMIN USERNAME
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-2649984098'))
+MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+Uux2GxpCKIxjNjU0')
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-2374228840').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv:learningbots.5yddr.mongodb.net/?retryWrites=true&w=majority&appName=learningbots")
-DATABASE_NAME = environ.get('DATABASE_NAME', "learningbots")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Filmdom:Filmdom@filmdom.mkkm0.mongodb.net/?retryWrites=true&w=majority&appName=Filmdom")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Filmdom")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
